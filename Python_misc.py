0eb2b803118188de6a2820a8092b17dea69faa8f
Alumasa <Admin@DESKTOP-CQ92B42>
2022-03-25 15:24:58 +0300

    Python code snippets

---
+++ Python_misc.py
@@ -0,0 +1,13 @@
+num_1 = int(input("Input the first number: "))
+
+num_2 = int(input("Input the second number: "))
+
+sq_num1 = num_1 * num_1
+sq_num2 = num_2 * num_2
+
+squares_add = sq_num1 + sq_num2
+
+print("The sum of the squares of ", num_1, "and ", num_2, "is: ", squares_add)
+
+
+
